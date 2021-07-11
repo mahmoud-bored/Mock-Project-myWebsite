b3584add200fc1b9e58361f0e67112fbc6ea4d98
mahmoud-bored <83987531+mahmoud-bored@users.noreply.github.com>
2021-07-11 12:44:19 +0300

    Add files via upload

--- main2.js
+++ main2.js
@@ -2,20 +2,27 @@
 
 
 // to turn the navbar to green when its clicked
-function activateNavBar(imageName) {
-    const whiteIcons = new Array("images/home.png", "images/about.png", "images/skills.png", "images/mywork.png", "images/email.png")
-    const iconsClasses = new Array('img.home', 'img.about', 'img.skills', 'img.mywork', 'img.email')
-    const navTextClasses = new Array('.text-hover-home', '.text-hover-about', '.text-hover-skills', '.text-hover-mywork', '.text-hover-email')
+function activateNavbar(imageName) {
+    const whiteIcons = new Array("images/home.png", "images/skills.png", "images/mywork.png", "images/email.png")
+    const iconsClasses = new Array('img.home', 'img.skills', 'img.mywork', 'img.email')
+    const navImgClasses = new Array('.img-hover-home', '.img-hover-skills', '.img-hover-mywork', '.img-hover-email')
+    const navTextClasses = new Array('.text-hover-home', '.text-hover-skills', '.text-hover-mywork', '.text-hover-email')
 
 
 
-    for(let i = 0; i <= 4; i++){
+    for(let i = 0; i <= 3; i++){
         document.querySelector(iconsClasses[i]).src = whiteIcons[i]
         document.querySelector(navTextClasses[i]).classList.remove('green-color')
+
+        document.querySelector(navImgClasses[i]).classList.remove('activate-img-hover')
+        document.querySelector(navTextClasses[i]).classList.remove('activate-text-hover')
     }
 
     document.querySelector(`img.${imageName}`).src = 'images/' + imageName + '-green.png'
     document.querySelector(`.text-hover-${imageName}`).classList.add('green-color')
+
+    document.querySelector(`.img-hover-${imageName}`).classList.add('activate-img-hover')
+    document.querySelector(`.text-hover-${imageName}`).classList.add('activate-text-hover')
 }
 //Ends
 
@@ -44,6 +51,45 @@ function hideCookiesBox() {
     setTimeout(function(){document.querySelector('.cookies-img').classList.add('block-display')}, 4000)
     setTimeout(function(){document.querySelector('.cookies-box').classList.add('hide-cookies-box')}, 2500)
     setTimeout(function(){document.querySelector('.cookies-box-border').classList.add('hide-cookies-box-border')}, 2500)
+    setTimeout(function(){document.querySelector('.cookies-box-blur').classList.add('hide-cookies-box-blur')}, 2700)
+    setTimeout(function(){document.querySelector('.cookies-box-blur').classList.add('block-display')}, 3200)
+
     setTimeout(function(){document.querySelector('.cookies-box-container').classList.add('block-display')}, 3000)
     
+    
+}
+
+
+// space moving background
+document.addEventListener("mousemove", parallax);
+function parallax(e){
+  document.querySelectorAll(".object").forEach(function(move){
+
+    var moving_value = move.getAttribute("data-value");
+    var x = (e.clientX * moving_value) / 250;
+    var y = (e.clientY * moving_value) / 250;
+
+    move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
+  });
+}
+
+// accordion panel
+function rotateChevron(className) {
+    document.querySelector('.chevron').classList.remove('click-rotate')
+
+    document.querySelector(`.${className}`).classList.add('click-rotate')
+}
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
 }
